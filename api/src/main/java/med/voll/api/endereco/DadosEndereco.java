eff4bd1d3780105b16e3d1c6446e0d2e753f229e
lucarcz <lucasrczz.01@gmail.com>
2023-06-13 16:00:27 -0300

    Equals e Getters adicionados

--- api/src/main/java/med/voll/api/endereco/DadosEndereco.java
+++ api/src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -19,6 +19,95 @@ public class DadosEndereco {
     }
 
     @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((logradouro == null) ? 0 : logradouro.hashCode());
+        result = prime * result + ((bairro == null) ? 0 : bairro.hashCode());
+        result = prime * result + ((cep == null) ? 0 : cep.hashCode());
+        result = prime * result + ((cidade == null) ? 0 : cidade.hashCode());
+        result = prime * result + ((uf == null) ? 0 : uf.hashCode());
+        result = prime * result + ((numero == null) ? 0 : numero.hashCode());
+        result = prime * result + ((complemento == null) ? 0 : complemento.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        DadosEndereco other = (DadosEndereco) obj;
+        if (logradouro == null) {
+            if (other.logradouro != null)
+                return false;
+        } else if (!logradouro.equals(other.logradouro))
+            return false;
+        if (bairro == null) {
+            if (other.bairro != null)
+                return false;
+        } else if (!bairro.equals(other.bairro))
+            return false;
+        if (cep == null) {
+            if (other.cep != null)
+                return false;
+        } else if (!cep.equals(other.cep))
+            return false;
+        if (cidade == null) {
+            if (other.cidade != null)
+                return false;
+        } else if (!cidade.equals(other.cidade))
+            return false;
+        if (uf == null) {
+            if (other.uf != null)
+                return false;
+        } else if (!uf.equals(other.uf))
+            return false;
+        if (numero == null) {
+            if (other.numero != null)
+                return false;
+        } else if (!numero.equals(other.numero))
+            return false;
+        if (complemento == null) {
+            if (other.complemento != null)
+                return false;
+        } else if (!complemento.equals(other.complemento))
+            return false;
+        return true;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    @Override
     public String toString() {
         return "DadosEndereco [logradouro=" + logradouro + ", bairro=" + bairro + ", cep=" + cep + ", cidade=" + cidade
                 + ", uf=" + uf + ", numero=" + numero + ", complemento=" + complemento + "]";
--- api/src/main/java/med/voll/api/medico/CadastroMedico.java
+++ api/src/main/java/med/voll/api/medico/CadastroMedico.java
@@ -37,6 +37,54 @@ public class CadastroMedico {
         return endereco;
     }
 
+
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((crm == null) ? 0 : crm.hashCode());
+        result = prime * result + ((especialidade == null) ? 0 : especialidade.hashCode());
+        result = prime * result + ((endereco == null) ? 0 : endereco.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CadastroMedico other = (CadastroMedico) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        if (crm == null) {
+            if (other.crm != null)
+                return false;
+        } else if (!crm.equals(other.crm))
+            return false;
+        if (especialidade != other.especialidade)
+            return false;
+        if (endereco == null) {
+            if (other.endereco != null)
+                return false;
+        } else if (!endereco.equals(other.endereco))
+            return false;
+        return true;
+    }
+
     @Override
     public String toString() {
         return "CadastroMedico [nome=" + nome + ", email=" + email + ", crm=" + crm + ", especialidade=" + especialidade
